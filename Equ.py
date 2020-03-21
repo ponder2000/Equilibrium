b6d3d78ee4dae70adb1cf6a7e52c87b185033cf5
Jay Saha <rajamrinalsaha2000@gmail.com>
2020-03-21 16:33:07 +0530

    Bug fixed

--- Equ.py
+++ Equ.py
@@ -1,35 +1,73 @@
+from tqdm import trange
 from Codes import *
+import json
+
+# list of queries
 Underlying_Stock = fno_list()
-a = 0
-while a < len(Underlying_Stock):
+
+# to store the searched result
+Underlying_Stock_with_value = dict()
+
+# scarping the data
+for a in trange(len(Underlying_Stock), desc = 'Number of Stock'):
     print(Underlying_Stock[a])
-    value = Option_Chain_Scrapper(str(Underlying_Stock[a]))[1]
+
+    # getting the values
+    value = Option_Chain_Scrapper(str(Underlying_Stock[a]))
     Strike_Price = value['Unnamed: 11_level_0']['Strike Price']
     call_oi = value['CALLS']['OI'].replace('-','0')
     put_oi = value['PUTS']['OI'].replace('-','0')
-    Strike_Price.to_list().pop()
-    call_oi.to_list().pop()
-    put_oi.to_list().pop()
-    #MAX PAIN CALCULATION
-    j = 0
+
+    # converting the pandas series into list for easy operation
+    Strike_Price = list(Strike_Price)
+    call_oi = list(call_oi)
+    put_oi = list(put_oi)
+
+    # removing the last total row
+    Strike_Price = Strike_Price[:len(Strike_Price)-1]
+    call_oi = call_oi[:len(call_oi)-1]
+    put_oi = put_oi[:len(put_oi)-1]
+
+    # converting into float for numerical calculation
+    Strike_Price = list(map(float, Strike_Price))
+    call_oi = list(map(float, call_oi))
+    put_oi = list(map(float, put_oi))
+
     values = []
-    while j < len(Strike_Price):
-        i = 0
-        l = 1
+    for j in range(len(Strike_Price)):
         instrinic_value = []
-        while i < len(Strike_Price):
+        for i in range(len(Strike_Price)):
             if Strike_Price[i] <= Strike_Price[j]:
                 instrinic_value.append(float(Strike_Price[j] - Strike_Price[i]) * float(call_oi[i]))
-                i += 1
             else:
                 instrinic_value.append(float(Strike_Price[i] - Strike_Price[j]) * float(put_oi[i]))
-                i += 1
         values.append(sum(instrinic_value))
-        j += 1
+
+
+    #MAX PAIN CALCULATION
     print("Max Pain Point")
-    #print(Strike_Price[values.index(min(values))])
+    try:
+        max_pain_point = Strike_Price[values.index(min(values))]
+    except:
+        print(f"No data available on required stock! length of value = {len(values)}\nHence making max_pain_point = 0")
+        max_pain_point = 0
+
     #Calculating PCR
-    #print("PCR:")
-    #print((sum(put_oi)/sum(call_oi)))
-    print('\n')
-    a += len(Underlying_Stock)
+    try:
+        pcr = sum(put_oi)/sum(call_oi)
+    except:
+        print(f"Either of put_oi or call_oi must be zero. len_put_oi, len_call_oi = {len(put_oi)}, {len(call_oi)}")
+        pcr = 0
+
+    # storing the values for further use
+    Underlying_Stock_with_value[Underlying_Stock[a]] = {'Max_Pain_Point':None, 'PCR': None, 'Values': None}
+    Underlying_Stock_with_value[Underlying_Stock[a]]['Max_Pain_Point'] = max_pain_point
+    Underlying_Stock_with_value[Underlying_Stock[a]]['PCR'] = pcr
+    Underlying_Stock_with_value[Underlying_Stock[a]]['Values'] = values
+
+
+# storing into a json object
+with open('Underlying_Stock_With_value.json', 'w') as f:
+    json.dump(Underlying_Stock_with_value, f)
+
+print("Scraping done!!!")
